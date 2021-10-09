bc38fcc6ffb7f434267c49ba702ce4c8a0afd9c9
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-10-09 19:55:09 +0530

    Update inline.py

--- WhisperBot/inline.py
+++ WhisperBot/inline.py
@@ -19,14 +19,14 @@ main = [
     InlineQueryResultArticle(
         title="Whisper Bot",
         input_message_content=InputTextMessageContent("Write Target User's @username or id at the end of your message."),
-        url="https://t.me/StarkBots",
+        url="https://t.me/storytym",
         description="Write Target User's @username or id at the end of your message.",
-        thumb_url="https://telegra.ph/file/33af12f457b16532e1383.jpg",
+        thumb_url="https://telegra.ph/file/3c4c427a5209208cd3f85.jpg",
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Learn More", url="https://t.me/WhisperStarkBot?start=start")],
+                [InlineKeyboardButton("Learn More", url="https://t.me/PooiV2_bot?start=start")],
                 [InlineKeyboardButton("🔒 Send a Whisper 🔒", switch_inline_query="")],
-                [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")]
+                [InlineKeyboardButton("♥ Movie Group ♥", url="https://t.me/movieshub_group")]
             ]
         ),
     )
@@ -81,9 +81,9 @@ async def previous_target(sender):
                   title=text1,
                   input_message_content=InputTextMessageContent(
                       f"A whisper message to {mention}" + " " + text2),
-                  url="https://t.me/StarkBots",
+                  url="https://t.me/storytym",
                   description=text2,
-                  thumb_url="https://telegra.ph/file/33af12f457b16532e1383.jpg",
+                  thumb_url="https://telegra.ph/file/3c4c427a5209208cd3f85.jpg",
                   reply_markup=InlineKeyboardMarkup(
                       [
                           [
@@ -152,9 +152,9 @@ async def answer(bot: Client, query):
                     InlineQueryResultArticle(
                         title=text1,
                         input_message_content=InputTextMessageContent(f"A whisper message to {target_user.mention}" + " " + text2),
-                        url="https://t.me/StarkBots",
+                        url="https://t.me/storytym",
                         description=text2,
-                        thumb_url="https://telegra.ph/file/33af12f457b16532e1383.jpg",
+                        thumb_url="https://telegra.ph/file/3c4c427a5209208cd3f85.jpg",
                         reply_markup=InlineKeyboardMarkup(
                             [
                                 [
